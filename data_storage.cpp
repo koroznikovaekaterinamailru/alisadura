82d50eeb186443ffe22f2c29c36666e6986c9ac7
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-03-07 17:28:27 +0600

    Create data_storage.cpp

---
+++ data_storage.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <iostrem>
+#include <fstream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+class DataStorage
+{
+    public:
+	    DataStorage();
+	    bool CreateObject (GameObject)
+		  bool DeleteObject (std:: string);
+		  Game Object* GetObject (std::string);
+};
+
