d03426c7c72ed266ecc2a703bbfead74d45f7b85
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-03-18 20:14:18 +0600

    Update gr_manager.cpp

--- gr_manager.cpp
+++ gr_manager.cpp
@@ -1,25 +1,66 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <vector>
+#include "game_object.h"
 
-class DrawableObj
+class GameObject;
+
+/*class DrawableObj
 {
   public:
-      virtual void draw(sf::RenderWindow* pointer);  
+      virtual void draw();  
       DrawableObj();
       ~DrawableObj();
-};
+};*/
 
 class GraphicsManager
 {
-public:
-    GraphicsManager();
+	public:
 
-    void drawObject(sf::DrawableObj& object);
-    void drawAllObjects(sf::RenderWindow* pointer);
+		sf::RenderWindow* getWindow();
 
-    void addRenderer(Renderer* renderer);
-    void removeRenderer(Renderer* renderer);
+	    void drawAllObjects(sf::RenderWindow& window);
+	    bool addObject(std::string name , GameObject* obj);
+	    bool removeObject(std::string name);
+	    void clear();
 
-    void clear();
+    private:
+
+        GraphicsManager() {}
+        std::map<std::string, GameObject*> drawable_obj;
+        sf::RenderWindow* window;
 };
 
+sf::RenderWindow* GraphicsManager::getWindow()
+{
+    return window;
+}
+
+void GraphicsManager::drawAllObjects(sf::RenderWindow& window)
+{
+    for (auto& object : drawable_obj)
+    {
+        object.second->getComponent<Renderer>()->draw(window);
+    }
+    window.display();
+};
+
+bool GraphicsManager::addObject(std::string name , GameObject* obj)
+{
+    if(drawable_obj[name] = obj)
+    {
+        return true;
+    }
+    return false;
+};
+
+bool GraphicsManager::removeObject(std::string name)
+{
+    if(drawable_obj.count(name) == 1)
+    {
+        delete drawable_obj[name];
+        drawable_obj.erase(name);
+        return true;
+    }
+    return false;
+};
