9370d6fe010c489527e24f02e5a6cd28d1307884
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-02 21:49:00 +0600

    Update data_storage.cpp

--- data_storage.cpp
+++ data_storage.cpp
@@ -1,15 +1,46 @@
+#ifndef DATA_STORAGE_H
+#define DATA_STORAGE_H
 #include <SFML/Graphics.hpp>
-#include <iostrem>
-#include <fstream>
+#include <iostream>
+#include "game_object.h"
 #include <vector>
+#include <map>
+
+class GameObject;
 
 class DataStorage
 {
     public:
 	    DataStorage();
-	    bool createObject(GameObject game_object);
+	    bool createObject(std::string name, GameObject* object);
 		bool deleteObject(std::string name);
-		Game Object* GetObject (std::string name);
+		GameObject* getObject(std::string name);
 	private:
-		std::vector<GameObject> objects;
+		std::map<std::string, GameObject*> objects;
 };
+
+bool DataStorage::createObject(std::string name , GameObject* object)
+{
+    if(objects[name] = object)
+      	return true;
+    else 
+    	return false;
+}
+
+bool DataStorage::deleteObject(std::string name)
+{
+    if(objects.count(name) == 1)
+    {
+        delete objects[name];
+        objects.erase(name);
+        return true;
+    }
+    return false;
+}
+
+GameObject* DataStorage::getObject(std::string name)
+{
+    return objects[name];
+}
+
+#endif//DATA_STORAGE_H
