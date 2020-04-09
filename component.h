06791c0007852a059056a42b327467c4838eb355
ergardt <55143723+ergardt@users.noreply.github.com>
2020-04-09 10:13:01 +0300

    New functions

--- component.h
+++ component.h
@@ -5,49 +5,66 @@
 #include <string>
 #include <vector>
 
-class GameObject;
-
-class GameObjectComponent//создание класса компонент
+namespace girls
 {
-    public:
-        GameObjectComponent();
-        virtual ~GameObjectComponent() = 0;
-        std::string name;
-        GameObject* owner;
-};
 
-class Renderer : public GameObjectComponent //отрисовка
-{
-    public:
-        Renderer();
-        void draw(sf::RenderWindow& window);
-        void loadTexture(std::string texture_name);
-        void createSprite();
-        sf::Sprite* getSprite();
+    class GameObject;
 
-    private:
-        sf::Texture texture;
-        sf::Sprite sprite;
-};
+    class GameObjectComponent//создание класса компонент
+    {
+        public:
+            GameObjectComponent();
+            virtual ~GameObjectComponent() = 0;
+            std::string name;
+            GameObject* owner;
+    };
 
-class Collider: public GameObjectComponent
-{   
-    public:
-        Collider();
-        bool isCollision();
-        float width;//ширина прямоугольника,в котором сожержится объект
-        float height;//высота прямоугольника,в котором сожержится объект
-        float x;
-        float y;
-    private:
-        std::vector<std::vector <float> > object_coordinate; //(координаты прямоугольника,в котором сожержится объект)
-};
+    class Renderer : public GameObjectComponent //отрисовка
+    {
+        public:
+            Renderer();
+            void draw(sf::RenderWindow& window)
+            {
+                sprite.setPosition( owner->position[0], owner->position[1] );
+                window.draw(sprite);
+            };
+            void loadTexture(std::string texture_name)
+            {
+                texture.loadFromFile(texturename);
+            };
+            void createSprite()
+            {
+                sprite.setTexture(texture);
+            };
+            sf::Sprite* getSprite()
+            {
+                return &sprite;
+            };
 
-class Script : public GameObjectComponent
-{
-    public:
-        Script();
-        virtual void update(float dt) {}
-};
+        private:
+            sf::Texture texture;
+            sf::Sprite sprite;
+    };
+
+    class Collider: public GameObjectComponent
+    {   
+        public:
+            Collider();
+            bool isCollision();
+            float width;//ширина прямоугольника,в котором сожержится объект
+            float height;//высота прямоугольника,в котором сожержится объект
+            float x;
+            float y;
+        private:
+            std::vector<std::vector <float> > object_coordinate; //(координаты прямоугольника,в котором сожержится объект)
+    };
+
+    class Script : public GameObjectComponent
+    {
+        public:
+            Script();
+            virtual void update(float dt) {}
+    };
+}
 
 #endif // COMPONENT_H
