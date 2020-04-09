e37624b42267fdb8643989a30360e7f4b20cd6d8
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-09 21:03:15 +0600

    Update game_object.h

--- game_object.h
+++ game_object.h
@@ -2,33 +2,53 @@
 #define GAMEOBJECT_H
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
 #include <vector>
 #include "component.h"
 #include "gr_manager.h"
 #include "script_manager.h"
 #include "physics_manager.h"
-#include "singleton.h"
+#include <typeinfo>
+
+class GraphicsManager;
+class PhysicsManager;
 
 class GameObject
 {
-    public:      
+    public:
 
         void setPosition(int x, int y);
+
+
         template <typename T>
         bool addComponent()//добавление новой компоненты для определённого GameObject 
         {
             T* obj = new T;
             components.push_back(obj);
             components[components.size() - 1]->owner = this;
-        };
+            /*GraphicsManager* GrManager = GraphicsManager::getInstance();
+            if(typeid(T).name() == typeid(Renderer).name())
+            {
+                GrManager->addObject(obj);
+            }
+            else if(typeid(T).name() == typeid(Script).name())
+            {
+                ScriptManager* ScriptManager = ScriptManager::getInstance();
+                ScriptManager -> addScript(obj);
+            }
+            else if(typeid(T).name() == typeid(Collider).name())
+            {
+                PhysicsManager* PhManager = PhysicsManager::getInstance();
+                PhManager -> addCollider(obj);
+            }*/
+        }
 
         template <typename T>
         T* getComponent()//обращение к данной компоненте
         {
+            std::string name = typeid(T).name();
             for (GameObjectComponent* c: components)
             {
-                if (c -> name == name)            
+                if (c->name == typeid(T).name())            
                 {
                     return static_cast<T*>(c);                   
                 }
@@ -39,6 +59,7 @@ class GameObject
         template <typename T>
         bool removeComponent()
         {
+            std::string name = typeid(T).name();
             for (int i = 0; i < components.size(); i++)
             {
                 if (components[i]->name == name)
@@ -53,7 +74,6 @@ class GameObject
     
     private:
         std::vector<GameObjectComponent*> components; 
-        std::string name;
 };
 
 #endif // GAMEOBJECT_H
