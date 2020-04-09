ea0aec04a6e765615173066aacd858201928a20e
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-09 13:39:34 +0600

    Update API_documentation.cpp

--- API_documentation.cpp
+++ API_documentation.cpp
@@ -1,22 +1,17 @@
 #ifndef API_H
 #define API_H
 
-namespace girls
-{
-	class GraphicsManager;
-	class PhysicsManager;
-	class ScriptManager;
-	class Singleton;
-	class DataStorage;
-	class GameObject;
+class GraphicsManager;
+class PhysicsManager;
+class ScriptManager;
+class Singleton;
+class DataStorage;
+class GameObject;
 		
-	class GameObjectComponent;
-	class Renderer;
+class GameObjectComponent;
+class Renderer;
 		
-
-	class Collider;
-	class Script;
-		
-}
+class Collider;
+class Script;
 
 #endif // API_H
