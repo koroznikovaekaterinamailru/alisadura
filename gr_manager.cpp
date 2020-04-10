acfb7ce4eb90ed037616067dbce0ed7d9e8433d9
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-10 15:58:38 +0600

    Create gr_manager.cpp

---
+++ gr_manager.cpp
@@ -0,0 +1,34 @@
+#include "gr_manager.h"
+
+GraphicsManager* GraphicsManager::getInstance()
+{
+    if (!instance)
+        instance = new GraphicsManager();
+    return instance;
+}
+GraphicsManager* GraphicsManager::instance = NULL;
+
+void GraphicsManager::addObject(GameObjectComponent* addingOfRenderer)
+{
+    drawable_obj.push_back(static_cast<Renderer*>(addingOfRenderer));
+};
+
+void GraphicsManager::removeObject(GameObjectComponent* removingOfRenderer)
+{
+    for (int i = 0; i < drawable_obj.size(); i++)
+    {
+        if (drawable_obj[i] == removingOfRenderer)
+        {
+            drawable_obj.erase(drawable_obj.begin() + i);
+        }
+    }
+};
+
+void GraphicsManager::drawAllObjects(sf::RenderWindow& window)
+{
+    for (Renderer* renderer : drawable_obj)
+    {
+        renderer-> draw(window);
+    }
+    window.display();
+};
