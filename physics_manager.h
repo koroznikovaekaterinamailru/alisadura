d2c440220d76e80ef2e0f625b53ba7495aae0709
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-10 16:00:20 +0600

    Update physics_manager.h

--- physics_manager.h
+++ physics_manager.h
@@ -14,66 +14,8 @@ class PhysicsManager
         void allCollisions();
 
     private:
-        std::vector <Collider*> collidingObjects;
         static PhysicsManager* instance;
+        std::vector <Collider*> collidingObjects;
 };
-
-PhysicsManager* PhysicsManager::instance = NULL;
-
-PhysicsManager* PhysicsManager::getInstance(){
-
-    if(!instance){
-        instance = new PhysicsManager;
-    }
-    return instance;
-}
-
-void PhysicsManager::addCollider(GameObjectComponent* addingOfCollider)
-{
-    collidingObjects.push_back(static_cast <Collider*> (addingOfCollider));
-}
-
-void PhysicsManager::removeCollider(GameObjectComponent* removingOfCollider)
-{
-    for(int i = 0 ; i < collidingObjects.size() ; i++)
-    {
-        if(collidingObjects[i] == removingOfCollider)
-        {
-            collidingObjects.erase(collidingObjects.begin() + i);
-        }
-    }
-}
-
-bool PhysicsManager::isCollision(Collider* object1 , Collider* object2)
-{       
-    bool XColl=false;
-    bool YColl=false;
-    if(((*object1).x + (*object1).width == (*object2).x) || ((*object2).x + (*object2).width == (*object1).x)) 
-        XColl = true;
-
-    if (((*object1).y + (*object1).height == (*object2).y) || ((*object2).y + (*object2).height == (*object2).y))
-    {
-        YColl = true;
-    }
-    if (XColl&YColl)
-    {
-        return true;
-    }
-    return false;
-}
-
-void PhysicsManager::allCollisions()
-{
-    for (int i = 0; i < collidingObjects.size(); i++)
-    {
-        for (int j = 0; j < collidingObjects.size(); j++)
-        {
-            if (i > j)
-            {
-                isCollision(collidingObjects[i], collidingObjects[j]);
-            }
-        }
-    }
-}
 
 #endif //PHYSICSMANAGER_H
