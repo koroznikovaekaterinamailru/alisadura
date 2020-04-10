5760227bc159d19cfd19d6acb1facc1903ae4e8a
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-10 16:02:20 +0600

    Create data_storage.cpp

---
+++ data_storage.cpp
@@ -0,0 +1,34 @@
+#include "data_storage.h"
+
+DataStorage* DataStorage::getInstance(){
+    
+    if(!instance){
+        instance = new DataStorage();
+    }
+    return instance;
+};
+DataStorage* DataStorage::instance = NULL;
+
+bool DataStorage::createObject(std::string name , GameObject* object)
+{
+    if(objects[name] = object)
+      	return true;
+    else 
+    	return false;
+}
+
+bool DataStorage::deleteObject(std::string name)
+{
+    if(objects.count(name) == 1)
+    {
+        delete objects[name];
+        objects.erase(name);
+        return true;
+    }
+    return false;
+}
+
+GameObject* DataStorage::getObject(std::string name)
+{
+    return objects[name];
+}
