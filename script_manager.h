beed0d185883bf7794f768798974e389cc639eb3
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-10 15:56:16 +0600

    Update script_manager.h

--- script_manager.h
+++ script_manager.h
@@ -15,43 +15,8 @@ class ScriptManager
 	    void removeScript(GameObjectComponent* removingOfScript);
 
 	private:
-	    std::vector<Script*> scripts;
         static ScriptManager* instance;
+	    std::vector<Script*> scripts;
 };
-
-ScriptManager* ScriptManager::getInstance()
-{
-    if (!instance)
-        instance = new ScriptManager();
-    return instance;
-}
-
-ScriptManager* ScriptManager::instance = NULL;
-
-void ScriptManager::update(float dt)
-{
-    for (Script* script : scripts)
-    {
-        script -> update(dt);
-    }
-};
-
-void ScriptManager::addScript(GameObjectComponent* addingOfScript)
-{
-    scripts.push_back(static_cast<Script*>(addingOfScript));
-}
-
-void ScriptManager::removeScript(GameObjectComponent* removingOfScript)
-{
-    for (int i = 0; i < scripts.size(); i++)
-    {
-        if (scripts[i] == removingOfScript)
-        {
-            scripts.erase(scripts.begin() + i);
-        }
-    }
-}
-
-ScriptManager* ScriptManager = NULL;
 
 #endif // SCRIPTS_H
