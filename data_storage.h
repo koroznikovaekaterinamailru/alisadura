5847243cb77cab11e584597c1cd1dee5c8a41278
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-10 16:01:47 +0600

    Update data_storage.h

--- data_storage.h
+++ data_storage.h
@@ -15,42 +15,8 @@ class DataStorage
 		GameObject* getObject(std::string name);
         static DataStorage* getInstance();
 	private:
-		std::map<std::string, GameObject*> objects;
         static DataStorage* instance;
+		std::map<std::string, GameObject*> objects;
 };
-
-DataStorage* DataStorage::getInstance(){
-    
-    if(!instance){
-        instance = new DataStorage();
-    }
-    return instance;
-};
-
-DataStorage* DataStorage::instance = NULL;
-
-bool DataStorage::createObject(std::string name , GameObject* object)
-{
-    if(objects[name] = object)
-      	return true;
-    else 
-    	return false;
-}
-
-bool DataStorage::deleteObject(std::string name)
-{
-    if(objects.count(name) == 1)
-    {
-        delete objects[name];
-        objects.erase(name);
-        return true;
-    }
-    return false;
-}
-
-GameObject* DataStorage::getObject(std::string name)
-{
-    return objects[name];
-}
 
 #endif//DATA_STORAGE_H
