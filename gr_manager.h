038579f899795971f1b76a7841c9de73784d546b
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-04-10 15:57:47 +0600

    Update gr_manager.h

--- gr_manager.h
+++ gr_manager.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include "game_object.h"
+#include "component.h"
 
 class GraphicsManager
 {
@@ -15,43 +16,9 @@ class GraphicsManager
         void drawAllObjects(sf::RenderWindow& window);
 
     private:
+        static GraphicsManager* instance;
         std::vector<Renderer*> drawable_obj; 
         sf::RenderWindow* window;
-        static GraphicsManager* instance;
 };
-
-GraphicsManager* GraphicsManager::getInstance()
-{
-    if (!instance)
-        instance = new GraphicsManager();
-    return instance;
-}
-
-void GraphicsManager::addObject(GameObjectComponent* addingOfRenderer)
-{
-    drawable_obj.push_back(static_cast<Renderer*>(addingOfRenderer));
-};
-
-void GraphicsManager::removeObject(GameObjectComponent* removingOfRenderer)
-{
-    for (int i = 0; i < drawable_obj.size(); i++)
-    {
-        if (drawable_obj[i] == removingOfRenderer)
-        {
-            drawable_obj.erase(drawable_obj.begin() + i);
-        }
-    }
-};
-
-void GraphicsManager::drawAllObjects(sf::RenderWindow& window)
-{
-    for (Renderer* renderer : drawable_obj)
-    {
-        renderer-> draw(window);
-    }
-    window.display();
-};
-
-GraphicsManager* GraphicsManager::instance = NULL;
 
 #endif // DRAWMANAGER_H
