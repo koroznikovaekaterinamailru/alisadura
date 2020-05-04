ec3e590690f7562f143188c81f8fef00deb10550
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-05-04 14:50:11 +0600

    Create component.cpp

---
+++ component.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "game_object.h"
+#include "component.h"
+
+////////////////////////////////////
+
+void Renderer::draw(sf::RenderWindow& window)
+{
+    sprite.setPosition( owner->position.x, owner->position.y );
+    window.draw(sprite);
+}
+////////////////////////////////////
+Script::Script(){
+    this->name = typeid(Script).name();
+}
