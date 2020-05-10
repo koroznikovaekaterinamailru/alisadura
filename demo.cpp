3b9ae58748e5437c7725f36169130a4e0d5634aa
ergardt <55143723+ergardt@users.noreply.github.com>
2020-05-10 13:37:50 +0300

    chase+collision

--- demo.cpp
+++ demo.cpp
@@ -2,14 +2,83 @@
 #include "script_manager.h"
 #include "data_storage.h"
 #include "physics_manager.h"
+#include "cmath"
 
-const float dt = 0.5;
+
+const float dt = 0.1;
 
 
+class Controller: public Script
+{
+public:
+    float speed = 1;
+
+    Controller()
+    {
+        name = typeid(*this).name();
+    }
+
+    void update(float dt)
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            owner->position.x += speed * dt;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            owner->position.x -= speed * dt;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            owner->position.y += speed * dt;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            owner->position.y -= speed * dt;
+        }
+    }
+};
+
+class MonsterAI: public Script
+{
+    public:
+        float speed = 1;
+        float cooldown = 0;
+
+    MonsterAI()
+    {
+        name = typeid(*this).name();
+    }
+
+    void update(float dt)
+    {
+        DataStorage* data_storage = DataStorage::getInstance();
+
+        GameObject* hero = data_storage->getObject("hero");
+
+        float rx = hero->position.x - owner->position.x;
+        float ry = hero->position.y - owner->position.y;
+
+        float distance = sqrt(rx*rx + ry*ry);
+
+        rx /= distance;
+        ry /= distance;
+
+        owner->position.x += rx * speed * dt;
+        owner->position.y += ry * speed * dt;
+
+        cooldown -= dt;
+        if ((cooldown <= 0) and (distance <= 20))
+        {
+            std::cout << "collision" << std::endl;
+            cooldown = 5;
+        }
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(720, 720), "girrrrrls", sf::Style::Close | sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode(720, 720), "girrrrrls");
     DataStorage* dataStorage = DataStorage::getInstance();
     GraphicsManager* GrManager = GraphicsManager::getInstance();
     ScriptManager* ScrManager = ScriptManager::getInstance();
@@ -20,42 +89,24 @@ int main()
     hero.getComponent<Renderer>()->loadTexture("hero_s.png");
     hero.getComponent<Renderer>()->createSprite();
     hero.setPosition(300,300);
-    hero.addComponent<KeyboardMove>();
+    hero.addComponent<Controller>();
     dataStorage->createObject("hero", &hero);
-    //std::cout<<"hero added"<<std::endl;
 
     GameObject monster;
     monster.addComponent<Renderer>();
     monster.getComponent<Renderer>()->loadTexture("monster_s.png");
     monster.getComponent<Renderer>()->createSprite();
-    monster.setPosition(200,100);
-    monster.addComponent<MonsterMove>();
+    monster.setPosition(600,100);
+    monster.addComponent<MonsterAI>();
     dataStorage->createObject("monster", &monster);
-    //std::cout<<"monster added"<<std::endl;
-
-
-    GameObject explosion;
-    explosion.addComponent<Renderer>();
-    explosion.getComponent<Renderer>()->loadTexture("explosion.png");
-    explosion.setPosition(200,100);
-    dataStorage->createObject("explosion", &explosion);
-    explosion.getComponent<Renderer>()->createSprite();
-    explosion.getComponent<Renderer>()->createAnimation(explosion.getComponent<Renderer>()->getTexture(), sf::Vector2f(4, 4), 0.1f, dt);
-
-    float deltaTime = 0.0f;
-    sf::Clock clock;
 
     sf::Event event;
-
     while (window.isOpen())
 	{
         window.clear(sf::Color(0,0,0));
         GrManager->drawAllObjects(window);
-        PhManager->allCollisions();
         window.display();
         ScrManager->update(dt);
-        GrManager->update(dt);
-
 
         while (window.pollEvent(event))
 	    {
@@ -67,6 +118,7 @@ int main()
 
     delete dataStorage;
     delete GrManager;
+    delete ScrManager;
     delete PhManager;
     return 0;
 }
