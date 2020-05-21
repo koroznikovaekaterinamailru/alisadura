2ee0577664f2df268228898767d21fda3d4ce8bf
aabraamoovaa <55063807+aabraamoovaa@users.noreply.github.com>
2020-05-21 14:06:11 +0600

    Update game.cpp

--- gg/game.cpp
+++ gg/game.cpp
@@ -15,14 +15,25 @@ class MoveByKeys:public Script
 
 void MoveByKeys::execute()
 {
+	float displacement = 3;
+	if ((this->obj->x < -300) or (this->obj->x > 300))
+	{
+		//this->obj->x = 0;
+		displacement = -displacement;
+	}
+	if ((this->obj->y < -300) or (this->obj->y > 300))
+	{
+		//this->obj->y = 0;
+		displacement = -displacement;
+	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		this->obj->x -=30;
+		this->obj->x -= displacement;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		this->obj->x +=30;
+		this->obj->x += displacement;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		this->obj->y +=30;
+		this->obj->y += displacement;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		this->obj->y -=30;
+		this->obj->y -= displacement;
 }
 
 class CustomCollider:public Collider{
@@ -31,8 +42,43 @@ class CustomCollider:public Collider{
 	}
 	~CustomCollider(){};
 };
+class Player
+{
+public:
+	std::string name;
+	std::string file_name;
+	Player(std::string obj_name, std::string file_name, float vx, float vy);
+	~Player();
+};
 
-int main(){
+Player::Player(std::string obj_name, std::string file_name, float vx, float vy)
+{
+	this->name = obj_name;
+	this->file_name = file_name;
+	ge::createObject(obj_name);
+	ge::addComponent<Renderer>(this->name);
+	ge::makeSprite(this->name, this->file_name);
+	ge::makeAnimation(this->name, 0, 0, 62.6, 89, 5, 0.02);
+	ge::addComponent<CustomCollider>(this->name);
+	std::vector<std::pair<float, float>> vec;
+	float x;
+	float y;
+	x = - 200;
+	y = -200;
+
+	ge::Vertex v = ge::Vertex(vec);
+	ge::makeModelOfCollider<CustomCollider>(v, this->name);
+	ge::setCoordinates(this->name,{x,y});
+	ge::setVelocity(this->name, vx, vy);
+	ge::addComponent<MoveByKeys>(this->name);
+}
+Player::~Player()
+{
+	ge::deleteObject(this->name);
+}
+
+int main()
+{
 	ge::setWindowTitle("Works!");
 	ge::setWindowSize(600, 600);
 
@@ -40,25 +86,30 @@ int main(){
 	ge::addComponent<Renderer>("back");
 	ge::makeSprite("back", "back2.png");
 
-	std::vector<std::pair<float, float>> vec;
+	/*std::vector<std::pair<float, float>> vec;
 	float x, y, vx, vy;
-	ge::createObject("horse");
-	ge::addComponent<Renderer>("horse");
-	ge::makeSprite("horse", "horse2.png");
-	ge::makeAnimation("horse", 0, 0, 127.6, 205, 3, 0.02);
-	ge::addComponent<CustomCollider>("horse");
+	ge::createObject("player");
+	ge::addComponent<Renderer>("player");
+	ge::makeSprite("player", "player_big.png");
+	ge::makeAnimation("player", 0, 0, 62.6, 89, 5, 0.02);
+	ge::addComponent<CustomCollider>("player");
 	ge::Vertex v = ge::Vertex(vec);
-	ge::makeModelOfCollider<CustomCollider>(v, "horse");
-
+	ge::makeModelOfCollider<CustomCollider>(v, "player");
 	x = -200;
 	y = -200;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		x +=30;
+	ge::setCoordinates("player",{x,y});
+	ge::addComponent<MoveByKeys>("player");*/
 
-	ge::setCoordinates("horse",{x,y});
-	
+	Player p = Player("player", "player_big.png", 0, 0);
 
-	
+	ge::createObject("cristal");
+	ge::addComponent<Renderer>("cristal");
+	ge::makeSprite("cristal", "cristal.png");
+	ge::makeAnimation("cristal", 0, 0, 32, 32, 8, 0.02);
+	float x_crystal = 100;
+	float y_crystal = -200;
+	ge::setCoordinates("cristal",{x_crystal,y_crystal});
+
 	ge::runApplication();
 	return 0;
 }
